7e8c10fd12879386b30f3d41406c0ad2f557f3ec
fwiedmann <privat@felixwiedmann.de>
2020-09-07 23:28:40 +0200

    feat(pk/cache): add a http request in-memory cache which implements the proxy.Cache interface

---
+++ pkg/cache/httpinmemory.go
@@ -0,0 +1,153 @@
+package cache
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/fwiedmann/prox/domain/entity/route"
+)
+
+type response struct {
+	body          []byte
+	contentLength int64
+	header        http.Header
+}
+
+const megaBytesToBytesMultiplier = 1e+6
+const httpInMemoryCacheHeader = "x-cached-by-prox"
+const httpContentTypeHeader = "Content-Type"
+
+// NewHTTPInMemoryCache creates a new http cache which stores http responses in memory
+func NewHTTPInMemoryCache(maxCacheSizeInMegaBytes int64) *HTTPInMemoryCache {
+	return &HTTPInMemoryCache{
+		store:               make(map[string]response),
+		maxCacheSizeInBytes: maxCacheSizeInMegaBytes * megaBytesToBytesMultiplier,
+	}
+}
+
+// HTTPInMemoryCache stores http responses in memory for a defined duration
+type HTTPInMemoryCache struct {
+	store               map[string]response
+	mtx                 sync.RWMutex
+	maxCacheSizeInBytes int64
+	cacheSizeInBytes    int64
+}
+
+// Get return a stored in memory response. If no response was found nil will be returned
+func (hc *HTTPInMemoryCache) Get(route route.Route, request *http.Request) *http.Response {
+	hc.mtx.RLock()
+	defer hc.mtx.RUnlock()
+
+	if val, ok := hc.store[buildID(route, request)]; ok {
+		if val.contentLength > 0 {
+			hc.cacheSizeInBytes -= val.contentLength
+		}
+		val.header.Set(httpInMemoryCacheHeader, "true")
+
+		return &http.Response{
+			Body:          ioutil.NopCloser(bytes.NewBuffer(val.body)),
+			ContentLength: val.contentLength,
+			Header:        val.header,
+		}
+	}
+	return nil
+}
+
+// Save a http request with its body in memory. The route.NameID, http.Request.Hostname and http.Request.RequestURI will be used to generate an ID.
+func (hc *HTTPInMemoryCache) Save(route route.Route, request *http.Request, resp *http.Response) {
+	if !hc.isValidateSave(route, request, resp) {
+		return
+	}
+
+	hc.mtx.Lock()
+	defer hc.mtx.Unlock()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		return
+	}
+
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	id := buildID(route, request)
+	hc.store[id] = response{
+		body:          body,
+		contentLength: resp.ContentLength,
+		header:        resp.Header,
+	}
+
+	if resp.ContentLength > 0 {
+		hc.cacheSizeInBytes += resp.ContentLength
+	}
+
+	go hc.deleteStoredResponseAfterTimeout(id, route.GetCacheTimeOut())
+}
+
+func (hc *HTTPInMemoryCache) isValidateSave(route route.Route, request *http.Request, resp *http.Response) bool {
+	if request.Method != http.MethodGet {
+		return false
+	}
+
+	if !isValidResponseCode(resp.StatusCode) {
+		return false
+	}
+
+	if hc.maxCacheSizeInBytes-hc.cacheSizeInBytes <= resp.ContentLength && hc.maxCacheSizeInBytes != -1 {
+		return false
+	}
+
+	if route.GetCacheMaxBodySizeInBytes() < resp.ContentLength && route.GetCacheMaxBodySizeInBytes() != -1 {
+		return false
+	}
+
+	if len(route.CacheAllowedContentTypes) != 0 && !isValidContentType(resp, route.CacheAllowedContentTypes) {
+		return false
+	}
+	return true
+}
+
+func (hc *HTTPInMemoryCache) deleteStoredResponseAfterTimeout(id string, duration time.Duration) {
+	time.Sleep(duration)
+	hc.mtx.Lock()
+	defer hc.mtx.Unlock()
+	hc.cacheSizeInBytes -= hc.store[id].contentLength
+	delete(hc.store, id)
+}
+
+func isValidResponseCode(code int) bool {
+	switch code {
+	case http.StatusOK:
+		return true
+	case http.StatusNotModified:
+		return true
+	default:
+		return false
+	}
+}
+
+func isValidContentType(r *http.Response, allowedContentTypes []string) bool {
+	requestContentType := r.Header.Get(httpContentTypeHeader)
+	if requestContentType == "" {
+		return false
+	}
+
+	for _, contentType := range allowedContentTypes {
+		if contentType == requestContentType {
+			return true
+		}
+	}
+	return false
+}
+
+func buildID(route route.Route, clientRequest *http.Request) string {
+	path := "/"
+	if clientRequest.RequestURI != "" {
+		path = clientRequest.RequestURI
+	}
+	return fmt.Sprintf("%s-%s-%s", route.NameID, clientRequest.Host, path)
+}
---
+++ pkg/cache/httpinmemory_test.go
@@ -0,0 +1,305 @@
+package cache
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/fwiedmann/prox/domain/entity/route"
+)
+
+func TestHTTPInMemoryCache_Get(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		store               map[string]response
+		mtx                 sync.RWMutex
+		maxCacheSizeInBytes int64
+		cacheSizeInBytes    int64
+	}
+	type args struct {
+		route   route.Route
+		request *http.Request
+	}
+	tests := []struct {
+		name               string
+		fields             fields
+		args               args
+		wantNil            bool
+		cacheSizeAfterExec int64
+	}{
+		{
+			name: "NilResponse",
+			fields: fields{
+				store:               map[string]response{},
+				mtx:                 sync.RWMutex{},
+				maxCacheSizeInBytes: 0,
+				cacheSizeInBytes:    2,
+			},
+			args: args{
+				route:   route.Route{NameID: "test-route"},
+				request: &http.Request{Host: "test.com", RequestURI: "/hello", ContentLength: 1},
+			},
+			wantNil:            true,
+			cacheSizeAfterExec: 2,
+		},
+		{
+			name: "ValidReturn",
+			fields: fields{
+				store:               map[string]response{"test-route-test.com-/hello": {contentLength: 1, header: map[string][]string{httpnMemoryCacheHeader: {"true"}}}},
+				mtx:                 sync.RWMutex{},
+				maxCacheSizeInBytes: 0,
+				cacheSizeInBytes:    2,
+			},
+			args: args{
+				route:   route.Route{NameID: "test-route"},
+				request: &http.Request{Host: "test.com", RequestURI: "/hello", ContentLength: 1},
+			},
+			wantNil:            false,
+			cacheSizeAfterExec: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hc := &HTTPInMemoryCache{
+				store:               tt.fields.store,
+				mtx:                 tt.fields.mtx,
+				maxCacheSizeInBytes: tt.fields.maxCacheSizeInBytes,
+				cacheSizeInBytes:    tt.fields.cacheSizeInBytes,
+			}
+			got := hc.Get(tt.args.route, tt.args.request)
+			if got != nil && tt.wantNil {
+				t.Errorf("Get() = %v, wantNil %v", got, tt.wantNil)
+			}
+
+			if tt.cacheSizeAfterExec != hc.cacheSizeInBytes {
+				t.Error("did not update cacheSizeAfterExec ")
+			}
+
+			if got != nil {
+				if got.Header.Get(httpnMemoryCacheHeader) == "" {
+					t.Error("Get() did not set the cache header")
+				}
+
+			}
+
+		})
+	}
+}
+
+func TestHTTPInMemoryCache_isValidateSave(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		store               map[string]response
+		mtx                 sync.RWMutex
+		maxCacheSizeInBytes int64
+		cacheSizeInBytes    int64
+	}
+	type args struct {
+		route   route.Route
+		request *http.Request
+		resp    *http.Response
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name:   "invalidMethod",
+			fields: fields{},
+			args: args{
+				request: &http.Request{Method: http.MethodPut},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 15, Hostname: "docker.com"},
+			},
+			want: false,
+		},
+		{
+			name:   "invalidMethod",
+			fields: fields{},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 400},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 15, Hostname: "docker.com"},
+			},
+			want: false,
+		},
+		{
+			name: "cacheIsFull",
+			fields: fields{
+				maxCacheSizeInBytes: 10,
+			},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 200, ContentLength: 100},
+				route:   route.Route{NameID: "test-route", Hostname: "docker.com"},
+			},
+			want: false,
+		},
+		{
+			name: "toBigBodySize",
+			fields: fields{
+				maxCacheSizeInBytes: 1110000000,
+			},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 200, ContentLength: 110000000},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 10, Hostname: "docker.com"},
+			},
+			want: false,
+		},
+		{
+			name: "invalidContentType",
+			fields: fields{
+				maxCacheSizeInBytes: 20,
+			},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 200, ContentLength: 10, Header: map[string][]string{httpContentTypeHeader: {"yaml"}}},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 15, Hostname: "docker.com", CacheAllowedContentTypes: []string{"json"}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hc := &HTTPInMemoryCache{
+				store:               tt.fields.store,
+				mtx:                 tt.fields.mtx,
+				maxCacheSizeInBytes: tt.fields.maxCacheSizeInBytes,
+				cacheSizeInBytes:    tt.fields.cacheSizeInBytes,
+			}
+
+			m := route.NewManager(route.NewInMemRepo())
+
+			if err := m.CreateRoute(context.Background(), &tt.args.route); err != nil {
+				t.Error(err)
+				return
+			}
+
+			routes, err := m.ListRoutes(context.Background())
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			var routeToUse *route.Route
+			for _, r := range routes {
+				if r.NameID == tt.args.route.NameID {
+					routeToUse = r
+				}
+			}
+			if routeToUse == nil {
+				t.Error("route is empty")
+				return
+			}
+
+			if got := hc.isValidateSave(*routeToUse, tt.args.request, tt.args.resp); got != tt.want {
+				t.Errorf("isValidateSave() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHTTPInMemoryCache_Save(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		store               map[string]response
+		mtx                 sync.RWMutex
+		maxCacheSizeInBytes int64
+		cacheSizeInBytes    int64
+	}
+	type args struct {
+		route   route.Route
+		request *http.Request
+		resp    *http.Response
+	}
+	tests := []struct {
+		name                string
+		fields              fields
+		args                args
+		storeCountAfterSave int
+		cacheSizeAfterSave  int64
+	}{
+
+		{
+			name: "InvalidSave",
+			fields: fields{
+				store:               map[string]response{},
+				mtx:                 sync.RWMutex{},
+				maxCacheSizeInBytes: 0,
+				cacheSizeInBytes:    0,
+			},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 200, ContentLength: 15000},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 10, Hostname: "docker.com"},
+			},
+			storeCountAfterSave: 0,
+			cacheSizeAfterSave:  0,
+		},
+		{
+			name: "ValidSave",
+			fields: fields{
+				store:               map[string]response{},
+				mtx:                 sync.RWMutex{},
+				maxCacheSizeInBytes: 1000000000000,
+				cacheSizeInBytes:    0,
+			},
+			args: args{
+				request: &http.Request{Method: http.MethodGet},
+				resp:    &http.Response{StatusCode: 200, ContentLength: 15000, Body: ioutil.NopCloser(bytes.NewBuffer([]byte{1}))},
+				route:   route.Route{NameID: "test-route", CacheMaxBodySizeInMegaBytes: 1000, Hostname: "docker.com"},
+			},
+			storeCountAfterSave: 1,
+			cacheSizeAfterSave:  15000,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			m := route.NewManager(route.NewInMemRepo())
+
+			if err := m.CreateRoute(context.Background(), &tt.args.route); err != nil {
+				t.Error(err)
+				return
+			}
+
+			routes, err := m.ListRoutes(context.Background())
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			var routeToUse *route.Route
+			for _, r := range routes {
+				if r.NameID == tt.args.route.NameID {
+					routeToUse = r
+				}
+			}
+			if routeToUse == nil {
+				t.Error("route is empty")
+				return
+			}
+
+			hc := &HTTPInMemoryCache{
+				store:               tt.fields.store,
+				mtx:                 tt.fields.mtx,
+				maxCacheSizeInBytes: tt.fields.maxCacheSizeInBytes,
+				cacheSizeInBytes:    tt.fields.cacheSizeInBytes,
+			}
+			hc.Save(*routeToUse, tt.args.request, tt.args.resp)
+
+			if tt.cacheSizeAfterSave != hc.cacheSizeInBytes {
+				t.Errorf("Save() cache size want %d, got %d", tt.cacheSizeAfterSave, hc.cacheSizeInBytes)
+			}
+
+			if tt.storeCountAfterSave != len(hc.store) {
+				t.Errorf("Save() store size want %d, got %d", tt.storeCountAfterSave, len(hc.store))
+			}
+
+		})
+	}
+}
